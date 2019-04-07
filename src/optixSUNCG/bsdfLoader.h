94d4d5ef67b0ae970dca8d833ca22adf140d0e6e
lzq <zhl378@eng.ucsd.edu>
2019-04-06 20:59:15 -0700

    Add ply reader

---
+++ src/optixSUNCG/bsdfLoader.h
@@ -0,0 +1,23 @@
+#ifndef BSDFLOADER_HEADER
+#define BSDFLOADER_HEADER 
+
+#include <optixu/optixpp_namespace.h>
+#include <optixu/optixu_aabb_namespace.h>
+#include <optixu/optixu_math_stream_namespace.h>
+#include "tinyxml.h"
+#include "sutil.h"
+#include "sutil/shapeStructs.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "constant.h"
+#include <cmath>
+#include <algorithm>
+
+
+
+#endif
